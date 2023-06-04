714fabd0e36f98354dffdf364f27186f581e98a2
Adir Elmakyes <adir@monkeytech.co.il>
2023-06-04 10:26:10 +0300

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -21,5 +21,3 @@ export * from './events/order-cancelled-event';
 export * from './events/order-created-event';
 export * from './events/expiration-complete-event';
 export * from './events/payment-created-event';
-
-// dasds
