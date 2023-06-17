953e9e3cb5771f16ca38b5980f356935b49a77ef
Adir Elmakyes <adir@monkeytech.co.il>
2023-06-17 19:43:45 +0300

    WIP

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -7,8 +7,7 @@ import {
   OrderStatus,
 } from '@aetickets1/common';
 
-import { Order } from '../../../../payments/src/models/order';
-
+import { Order } from '../../models/order';
 export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
   subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
   queueGroupName: string = queueGropName;
@@ -23,6 +22,7 @@ export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
     order.set({
       status: OrderStatus.Complete,
     });
+    await order.save();
 
     msg.ack();
   }
